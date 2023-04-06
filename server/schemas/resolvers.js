5c2153b301bc8b1e5e53c4d88654be97d5b84c21
mattchief4234 <mattchief4234@gmail.com>
2023-04-05 21:41:34 -0600

    created resolvers and type defs
    I have added thses files and they will need to be reviewed and revised soon!

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,45 @@
+const { Project } = require('../models')
+
+const resolvers = {
+    Query: {
+        projects: async () => {
+            return Project.find().sort({createdAt: -1 })
+        },
+
+        project: async (parent, { projectId }) => {
+            return Project.findOne({_id: ProjectId })
+        },
+
+    }, //end of Quesry
+
+    Mutation: {
+        addProject: async(parent, {projectText, }) => {
+            return Project.create({ projectText })
+        },
+        addComment: async(parent, { projectId, commentText }) => {
+            return Project.findOneAndUpdate(
+                { _id: projectId },
+                {
+                    $addtoSet: {comments: { commentText } },
+                },
+                {
+                    new: true,
+                    runValidators: true,
+                },
+            );
+            
+        },
+        removeProject: async (parent, { projectId} ) => {
+            return Project.findOneAndDelete({ _id: thoughtId })
+        },
+        removeComment: async (parent, { projectId, commentId }) => {
+            return Project.findOneAndDelete(
+                { _id: projectId},
+                { $pull: { comments: { _id: commentId} } },
+                { new: true }
+            );
+        }
+    } //end of Mutation
+};
+
+module.exports = resolvers;
