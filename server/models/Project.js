ddf5d92715bd6f04630520ff70ea1aee71851e50
mattchief4234 <mattchief4234@gmail.com>
2023-04-05 20:16:55 -0600

    updated the project model
    hopefully this works

--- server/models/Project.js
+++ server/models/Project.js
@@ -7,12 +7,36 @@ const projectSchema = new Schema({
     unique: true,
     trim: true,
   },
-  skills: [
+  createdAt: [
     {
-      type: String,
-      trim: true,
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => dateFormat(timestamp)
     },
   ],
+  projectText: {
+    type: String,
+    required: true,
+    minlength: 60,
+    maxlength: 500,
+    trim: true,
+  },
+  comments: [
+    {
+      commentText: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 280,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (timestamp) => dateFormat(timestamp),
+      },
+    },
+  ],
+ 
 });
 
 const Project = model('Project', projectSchema);
