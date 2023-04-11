a1463bdf9b8dd129c3aa4515942b7290b46fe423
mattchief4234 <mattchief4234@gmail.com>
2023-04-10 19:49:03 -0600

    might work with npm start develop

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 
-const {  } = require('./schemas'); //NEED TO INPUT WHICH SCHEMAS THAT WE NEED TO ADD THEM WHEN WE HAVE THEM!
+const { resolvers, typeDefs  } = require('./schemas'); //NEED TO INPUT WHICH SCHEMAS THAT WE NEED TO ADD THEM WHEN WE HAVE THEM!
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
